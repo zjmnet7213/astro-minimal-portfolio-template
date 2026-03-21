ff91800d77729d6282f29f1969abd18fc1d565a8
Tim Witzdam <tim2013w@gmail.com>
2026-03-21 08:59:13 +0100

    CHORE: Update dependencies

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,7 @@
 // Import utilities from `astro:content`
-import { z, defineCollection } from "astro:content";
+import { z } from "astro/zod";
 import { glob } from "astro/loaders";
+import { defineCollection } from "astro:content";
 
 // Define a `type` and `schema` for each collection
 const postsCollection = defineCollection({
